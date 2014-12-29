06709de2d46d05b56264f2ee9b04158da900ba3d
Patrick Dockhorn <patrick@apptastic.com.au>
2014-12-29 15:09:23 +1100

    first version

---
+++ AWSV2TestTests/AWSV2TestTests.m
@@ -0,0 +1,40 @@
+//
+//  AWSV2TestTests.m
+//  AWSV2TestTests
+//
+//  Created by Patrick Dockhorn on 29/12/2014.
+//  Copyright (c) 2014 AppTastic. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface AWSV2TestTests : XCTestCase
+
+@end
+
+@implementation AWSV2TestTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
